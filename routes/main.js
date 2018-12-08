2823c328544d772294a1470f0cc3563dcadd6214
Albert Camps Oller <campsupc@gmail.com>
2018-12-08 12:31:01 +0100

    changes workflow

--- routes/main.js
+++ routes/main.js
@@ -11,14 +11,32 @@ module.exports = function (passport) {
 		res.render('login')
 	})
 
-	router.get('/learn/vulnerability/:vuln', authHandler.isAuthenticated, function (req, res) {
-		res.render('vulnerabilities/layout', {
+	router.get('/learn/hacking-web/vulnerability/:vuln', authHandler.isAuthenticated, function (req, res) {
+		res.render('hacking-web-vulnerabilities/layout', {
 			vuln: req.params.vuln,
-			vuln_title: vulnDict[req.params.vuln],
+			vuln_title: vulnDict['hackingWeb'][req.params.vuln],
 			vuln_scenario: req.params.vuln + '/scenario',
 			vuln_description: req.params.vuln + '/description',
 			vuln_reference: req.params.vuln + '/reference',
-			vulnerabilities:vulnDict
+			vulnerabilities:vulnDict['hackingWeb']
+		}, function (err, html) {
+			if (err) {
+				console.log(err)
+				res.status(404).send('404')
+			} else {
+				res.send(html)
+			}
+		})
+	})
+
+	router.get('/learn/cryptography/vulnerability/:vuln', authHandler.isAuthenticated, function (req, res) {
+		res.render('cryptography-vulnerabilities/layout', {
+			vuln: req.params.vuln,
+			vuln_title: vulnDict['cryptography'][req.params.vuln],
+			vuln_scenario: req.params.vuln + '/scenario',
+			vuln_description: req.params.vuln + '/description',
+			vuln_reference: req.params.vuln + '/reference',
+			vulnerabilities:vulnDict['cryptography']
 		}, function (err, html) {
 			if (err) {
 				console.log(err)
@@ -30,7 +48,18 @@ module.exports = function (passport) {
 	})
 
 	router.get('/learn', authHandler.isAuthenticated, function (req, res) {
-		res.render('learn',{vulnerabilities:vulnDict})
+		console.log(vulnDict);
+		const keysHackingWeb = Object.keys(vulnDict['hackingWeb']);
+		const mainKeyHackingWeb = keysHackingWeb[0];
+
+		const keysCryptography = Object.keys(vulnDict['cryptography']);
+		const mainKeyCryptography = keysCryptography[0];
+		
+		res.render('learn',{
+			vulnerabilities:vulnDict['hackingWeb'],
+			hackingWebMainVulnerability: mainKeyHackingWeb,
+			cryptographyMainVulnerability: mainKeyCryptography
+		})
 	})
 
 	router.get('/register', authHandler.isNotAuthenticated, function (req, res) {
